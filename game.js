55d2095fef07c58d4bf0467a2deb627252e6f96e
Peter Tountas <petertountas1@gmail.com>
2016-10-28 14:42:36 -0500

    ugh

---
+++ game.js
@@ -0,0 +1,15 @@
+
+
+// var managerFight = {
+// 	maddonhealth: 100,
+// 	titohealth: 100,
+// 	maddonAttack: function(){
+// 		$('#throwPizza').on('click', function(){
+// 			$('#maddonAttack').html('<p>Maddon throws Pizza</p>');
+// 		});
+// 	}
+// };
+
+$('throwPizza').on('click',function(){
+	$('maddonAttack').html("<p>Maddon thows pizza</p>")
+});
